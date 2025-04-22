dc954f6c36f00885fdcd9777dcc90ecde902c8ba
TotemLi <1027029629@qq.com>
2025-04-22 21:13:12 +0800

    队列

--- task/main/task.c
+++ task/main/task.c
@@ -18,17 +18,40 @@ void taskProducer(void *param) {
     int i = 0;
     while (1)
     {
-        if (xQueueSend(queue, &i, 0)!=pdPASS) {
+        if (xQueueSend(queue, &i, 0)==pdPASS) {
             ESP_LOGI(TAG, "send success");
+        } else {
+            ESP_LOGI(TAG, "send fail");
         }
+        i++;
+        vTaskDelay(1000/portTICK_PERIOD_MS);
     }
-    
+}
+
+void taskConsume(void *param) {
+    QueueHandle_t queue = (QueueHandle_t)param;
+
+    while (1)
+    {
+        int data;
+        if (xQueueReceive(queue, &data, 0)==pdPASS) {
+            ESP_LOGI(TAG, "receve data: %d", data);
+        }
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
 }
 
 void app_main(void)
 {
-    TaskHandle_t myTaskA;
-    xTaskCreatePinnedToCore(taskA, "taskA", 2048, NULL, 1, &myTaskA, 1);
-    vTaskDelay(pdMS_TO_TICKS(5*500));
-    vTaskDelete(myTaskA);
+    // TaskHandle_t myTaskA;
+    // xTaskCreatePinnedToCore(taskA, "taskA", 2048, NULL, 1, &myTaskA, 1);
+    // vTaskDelay(pdMS_TO_TICKS(5*500));
+    // vTaskDelete(myTaskA);
+    QueueHandle_t queue = xQueueCreate(10, sizeof(int));
+    if (queue==NULL) {
+        ESP_LOGI(TAG, "create queue err");
+        return;
+    }
+    xTaskCreate(taskProducer, "producer", 2048, queue, 1, NULL);
+    xTaskCreate(taskConsume, "consume", 2048, queue, 1, NULL);
 }
