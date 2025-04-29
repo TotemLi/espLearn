bc18513744a05aee53762064722128f1f5a7be7f
TotemLi <1027029629@qq.com>
2025-04-29 18:00:52 +0800

    small

--- uart/main/uart.c
+++ uart/main/uart.c
@@ -2,8 +2,14 @@
 #include <string.h>
 #include "driver/uart.h"
 #include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
 
 const uart_port_t uart_num = UART_NUM_2;
+QueueHandle_t uart_queue;
+#define BUF_SIZE (1024)
+#define RD_BUF_SIZE (BUF_SIZE)
 
 void uart_init(void)
 {
@@ -19,7 +25,6 @@ void uart_init(void)
     ESP_ERROR_CHECK(uart_set_pin(uart_num, 4, 5, 18, 19));
 
     const int uart_buffer_size = 2048;
-    QueueHandle_t uart_queue;
     ESP_ERROR_CHECK(uart_driver_install(uart_num, uart_buffer_size, uart_buffer_size, 10, &uart_queue, 0));
 }
 
@@ -28,24 +33,47 @@ void send_data(char *data)
     uart_write_bytes(uart_num, data, strlen(data));
 }
 
-char *receve_data()
+static void uart_event_task(void *pvParam)
 {
-    uint8_t data[128];
-    int len = 0;
-    ESP_ERROR_CHECK(uart_get_buffered_data_len(uart_num, (size_t *)&len));
-    if (len == 0)
+    uart_event_t event;
+    size_t buffer_size;
+    uint8_t *dtmp = (uint8_t *)malloc(RD_BUF_SIZE);
+    for (;;)
     {
-        return NULL;
+        if (xQueueReceive(uart_queue, (void *)&event, (TickType_t)portMAX_DELAY))
+        {
+            bzero(dtmp, RD_BUF_SIZE);
+            switch (event.type)
+            {
+            case UART_DATA:
+                uart_read_bytes(uart_num, dtmp, event.size, portMAX_DELAY);
+                ESP_LOGI("uart", "data: %s", dtmp)
+                break;
+            default:
+                break;
+            }
+        }
     }
-    len = uart_read_bytes(uart_num, data, len, pdMS_TO_TICKS(1000));
-    char *buffer = (char *)malloc(len + 1);
-    for (int i = 0; i < len; i++)
-    {
-        buffer[i] = (char)data[i];
-    }
-    buffer[len] = '\0';
-    return buffer;
 }
+
+// char *receve_data()
+// {
+//     uint8_t data[128];
+//     int len = 0;
+//     ESP_ERROR_CHECK(uart_get_buffered_data_len(uart_num, (size_t *)&len));
+//     if (len == 0)
+//     {
+//         return NULL;
+//     }
+//     len = uart_read_bytes(uart_num, data, len, pdMS_TO_TICKS(1000));
+//     char *buffer = (char *)malloc(len + 1);
+//     for (int i = 0; i < len; i++)
+//     {
+//         buffer[i] = (char)data[i];
+//     }
+//     buffer[len] = '\0';
+//     return buffer;
+// }
 
 void app_main(void)
 {
