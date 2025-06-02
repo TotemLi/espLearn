b5338e2b894a0da444d7529a6dac6c3cd56598dd
TotemLi <1027029629@qq.com>
2025-06-02 22:02:18 +0800

    small

--- lcd/main/lcd.c
+++ lcd/main/lcd.c
@@ -1,7 +1,90 @@
 #include <stdio.h>
+#include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_heap_caps.h"
+#include "driver/spi_master.h"
+#include "driver/gpio.h"
+
+#define LCD_HOST SPI2_HOST
+#define PARALLEL_LINES 30
+#define ROTATE_FRAME 30
+
+#define LCD_PIXEL_CLOCK_HZ (20 * 1000 * 1000)
+#define LCD_BK_LIGHT_ON_LEVEL 1
+#define LCD_BK_LIGHT_OFF_LEVEL !LCD_BK_LIGHT_ON_LEVEL
+
+#define PIN_NUM_MOSI 7
+#define PIN_NUM_SCK 6
+#define PIN_NUM_CS 17
+#define PIN_NUM_DC 15
+#define PIN_NUM_RST 16
+#define PIN_NUM_BK_LIGHT 5
+
+#define LCD_H_RES 480
+#define LCD_V_RES 320
+#define LCD_CMD_BITS 8
+#define LCD_PARAM_BITS 8
 
 void app_main(void)
 {
+    gpio_config_t bk_gpio_config = {
+        .mode = GPIO_MODE_OUTPUT,
+        .pin_bit_mask = 1ULL << PIN_NUM_BK_LIGHT,
+    };
+    ESP_ERROR_CHECK(gpio_config(&bk_gpio_config));
+
+    spi_bus_config_t buscfg = {
+        .sclk_io_num = PIN_NUM_SCK,
+        .mosi_io_num = PIN_NUM_MOSI,
+        .miso_io_num = -1,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = PARALLEL_LINES * LCD_H_RES * 2 + 8,
+    };
+    ESP_ERROR_CHECK(spi_bus_initialize(LCD_HOST, &buscfg, SPI_DMA_CH_AUTO));
+
+    esp_lcd_panel_io_handle_t io_handle = NULL;
+    esp_lcd_panel_io_spi_config_t io_config = {
+        .dc_gpio_num = PIN_NUM_DC,
+        .cs_gpio_num = PIN_NUM_CS,
+        .pclk_hz = LCD_PIXEL_CLOCK_HZ,
+        .lcd_cmd_bits = LCD_CMD_BITS,
+        .lcd_param_bits = LCD_PARAM_BITS,
+        .spi_mode = 0,
+        .trans_queue_depth = 10,
+    };
+
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_HOST, &io_config, &io_handle));
+
+    esp_lcd_panel_handle_t panel_handle = NULL;
+    esp_lcd_panel_dev_config_t panel_config = {
+        .reset_gpio_num = PIN_NUM_RST,
+        .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_RGB,
+        .bits_per_pixel = 16,
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_panel_st7789(io_handle, &panel_config, &panel_handle));
+
+    // Turn off backlight to avoid unpredictable display on the LCD screen while initializing
+    // the LCD panel driver. (Different LCD screens may need different levels)
+    ESP_ERROR_CHECK(gpio_set_level(PIN_NUM_BK_LIGHT, LCD_BK_LIGHT_OFF_LEVEL));
+
+    // Reset the display
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
+
+    // Initialize LCD panel
+    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
+
+    // Turn on the screen
+    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
+    ESP_ERROR_CHECK(esp_lcd_panel_invert_color(panel_handle, true));
+
+    // // Swap x and y axis (Different LCD screens may need different options)
+    // ESP_ERROR_CHECK(esp_lcd_panel_swap_xy(panel_handle, true));
+
+    // Turn on backlight (Different LCD screens may need different levels)
+    ESP_ERROR_CHECK(gpio_set_level(PIN_NUM_BK_LIGHT, LCD_BK_LIGHT_ON_LEVEL));
 }
